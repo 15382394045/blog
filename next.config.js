b4bb1f9aa765f7581deaec003039e68bc1ffc638
15382394045 <2465381057@qq.com>
2025-04-10 21:11:05 +0800

    Fix build configuration and simplify deployment

--- next.config.js
+++ next.config.js
@@ -2,16 +2,9 @@
 const nextConfig = {
   output: 'export',  // 启用静态导出
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
     unoptimized: true,  // 为了静态导出
   },
   basePath: '/blog',  // 设置基础路径为你的仓库名
-  assetPrefix: '/blog/',
   trailingSlash: true,
 }
 
